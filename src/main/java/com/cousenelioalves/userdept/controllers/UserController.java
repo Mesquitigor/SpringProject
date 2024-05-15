4e7255303eba40e82ac72ace03b652c884cc5cdd
Higor Mesquita <higormesquita@gmail.com>
2024-05-14 23:31:31 -0300

    second commit

--- src/main/java/com/cousenelioalves/userdept/controllers/UserController.java
+++ src/main/java/com/cousenelioalves/userdept/controllers/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,4 +47,11 @@ public class UserController {
 		return ("User deleted successfully");
 	}
 	
+	@PatchMapping(value = "/patch/{id}")
+	public User patchUser(@PathVariable Long id, @RequestBody User user) {
+		User result = repository.findById(id).get();
+		result.setName(user.getName());
+		result.setEmail(user.getEmail());
+		return repository.save(result);
+	}
 }
